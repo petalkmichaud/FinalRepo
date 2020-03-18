e9ac42ae3cefbce32b870929d462ec52c19c29f4
Sithale <61808802+Sithale@users.noreply.github.com>
2020-03-18 00:45:31 -0700

    Add files via upload

--- SurpriseAttack.java
+++ SurpriseAttack.java
@@ -3,28 +3,38 @@ public class SurpriseAttack implements Ability
 {
 	GameCharacter thisChar;
 	
+	
+	
 	public String getDisplay()
 	{
-		return " uses the special move surprise attack";
+		return "Surprise Attack";
 	}
 	
 	public void doAbility(GameCharacter opponent) 
 	{
+		
+		//Method does not work, how to change to work with our structure?
 		double surprise = Math.random();
 		if (surprise <= thisChar.getChanceToHit())
 		{
+			
 			System.out.println("Surprise attack was successful!\n" +
-								thisChar.getName() + " gets an additional turn.");
-			//numTurns++;
+								thisChar.getName()+ " gets an additional turn.");
 			thisChar.attack(opponent);
+			
+			//Pick battle option
+			
 		}//end surprise
 		else if (surprise >= .9)
 		{
 			System.out.println("Uh oh! " + opponent.getName() + " saw you and" +
 								" blocked your attack!");
 		}
-		else
-		    thisChar.attack(opponent);
+		
+		thisChar.attack(opponent);
+	}
+	public void setThisChar(GameCharacter thisChar) {
+		this.thisChar = thisChar;
 	}
 
 }
